a7855744e8eb7893b8d94ddc9f0158e29ade8b0e
federicorojas11 <federico.rojas.121@gmail.com>
2021-08-09 14:32:37 -0300

    solucionado error http module not found - agregado middleware acceso cors

--- app.js
+++ app.js
@@ -15,6 +15,18 @@ app.use(
 );
 app.use(bodyParser.json());
 
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
 app.use(apiPrefix + "/users", require("./routes/user.route"));
 app.use(apiPrefix + "/countries", require("./routes/countries.route"));
 app.use(apiPrefix + "/login", require("./routes/login.route"));
